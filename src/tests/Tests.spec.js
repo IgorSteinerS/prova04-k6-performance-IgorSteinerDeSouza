1dfd63730a0df6cef9b4a9b984bb38189ac0f8ca
IgorSteiner <igorsteinerdesouza@gmail.com>
2025-11-18 19:00:02 -0300

    first commit

---
+++ src/tests/Tests.spec.js
@@ -0,0 +1,77 @@
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Trend, Rate } from 'k6/metrics';
+
+export const searchDuration = new Trend('search_anime_duration', true);
+export const detailsDuration = new Trend('get_details_duration', true);
+export const charactersDuration = new Trend('get_characters_duration', true);
+
+export const RateContentOK = new Rate('content_OK');
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.10'],
+
+    search_anime_duration: ['p(95)<2000'],
+    content_OK: ['rate>0.90']
+  },
+  stages: [
+    { duration: '10s', target: 1 },
+    { duration: '20s', target: 2 },
+    { duration: '10s', target: 0 }
+  ]
+};
+
+export function handleSummary(data) {
+  return {
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
+  };
+}
+
+export default function () {
+  const baseUrl = 'https://api.jikan.moe/v4';
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const OK = 200;
+
+  const resSearch = http.get(`${baseUrl}/anime?q=Re:Zero&limit=1`, params);
+
+  searchDuration.add(resSearch.timings.duration);
+  RateContentOK.add(resSearch.status === OK);
+
+  check(resSearch, {
+    'GET Search "Re:Zero" - Status 200': () => resSearch.status === OK
+  });
+
+  sleep(1);
+
+  const animeId = 31240;
+  const resDetails = http.get(`${baseUrl}/anime/${animeId}`, params);
+
+  detailsDuration.add(resDetails.timings.duration);
+  RateContentOK.add(resDetails.status === OK);
+
+  check(resDetails, {
+    'GET Anime Details - Status 200': () => resDetails.status === OK
+  });
+
+  sleep(1);
+
+  const resChars = http.get(`${baseUrl}/anime/${animeId}/characters`, params);
+
+  charactersDuration.add(resChars.timings.duration);
+  RateContentOK.add(resChars.status === OK);
+
+  check(resChars, {
+    'GET Anime Characters - Status 200': () => resChars.status === OK
+  });
+
+  sleep(2);
+}
